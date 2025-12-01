1c7c8bdf8ef8040a28dcb26a472cbaee83220e7d
huni-na <huni.na@soomai.life>
2025-12-01 14:39:50 +0900

    remove service temp slide

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import HeroSection from "@/components/HeroSection";
 import AboutSection from "@/components/AboutSection";
-import ServiceGrid from "@/components/ServiceGrid";
 
 export default function Home() {
   return (
     <div className="flex flex-col">
       <HeroSection />
       <AboutSection />
-      <ServiceGrid />
     </div>
   );
 }
